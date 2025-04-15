36b9485f06dbeeeb2d95fb9e3dcf75952d2a4b8c
igorBys <stevejobs69@MacBook-Air-steve.local>
2025-04-15 13:19:08 +0300

    added remote demoqa

--- src/test/java/lesson11/tests/PracticeFormTest.java
+++ src/test/java/lesson11/tests/PracticeFormTest.java
@@ -1,41 +1,50 @@
 package lesson11.tests;
 
 import lesson11.pages.PracticeFormPage;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class PracticeFormTest extends TestBase {
     TestData td = new TestData();
-
     PracticeFormPage practiceFormPage = new PracticeFormPage();
 
     @Test
+    @Tag("demoqa")
     void fillFormTest() {
-        practiceFormPage.openPage()
-                .closeBanners()
-                .setFirsName(td.firstName)
-                .setLastName(td.lastName)
-                .setEmail(td.email)
-                .setGender(td.gender)
-                .setUserNumber(td.userNumber)
-                .setDateOfBirth(td.day, td.month, td.year)
-                .setSubjects(td.subjects)
-                .setHobbies(td.hobbies)
-                .setUploadPicture(td.uploadPicture)
-                .setCurrentAddress(td.currentAddress)
-                .setState(td.state)
-                .setCity(td.city)
-                .setSubmit()
-                .isResultWindowDisplayed("Thanks for submitting the form")
-                .checkResult("Student Name", td.firstName + " " + td.lastName)
-                .checkResult("Student Email", td.email)
-                .checkResult("Gender", td.gender)
-                .checkResult("Mobile", td.userNumber)
-                .checkResult("Date of Birth", td.day + " " + td.month + "," + td.year)
-                .checkResult("Subjects", td.subjects)
-                .checkResult("Hobbies", td.hobbies)
-                .checkResult("Picture", td.uploadPicture)
-                .checkResult("Address", td.currentAddress)
-                .checkResult("State and City", td.state + " " + td.city);
+        step("Open form", () -> {
+            practiceFormPage.openPage()
+                    .closeBanners();
+        });
+        step("Fill form", () -> {
+            practiceFormPage.setFirsName(td.firstName)
+                    .setLastName(td.lastName)
+                    .setEmail(td.email)
+                    .setGender(td.gender)
+                    .setUserNumber(td.userNumber)
+                    .setDateOfBirth(td.day, td.month, td.year)
+                    .setSubjects(td.subjects)
+                    .setHobbies(td.hobbies)
+                    .setUploadPicture(td.uploadPicture)
+                    .setCurrentAddress(td.currentAddress)
+                    .setState(td.state)
+                    .setCity(td.city)
+                    .setSubmit();
+        });
+        step("Verify results", () -> {
+            practiceFormPage.isResultWindowDisplayed("Thanks for submitting the form")
+                    .checkResult("Student Name", td.firstName + " " + td.lastName)
+                    .checkResult("Student Email", td.email)
+                    .checkResult("Gender", td.gender)
+                    .checkResult("Mobile", td.userNumber)
+                    .checkResult("Date of Birth", td.day + " " + td.month + "," + td.year)
+                    .checkResult("Subjects", td.subjects)
+                    .checkResult("Hobbies", td.hobbies)
+                    .checkResult("Picture", td.uploadPicture)
+                    .checkResult("Address", td.currentAddress)
+                    .checkResult("State and City", td.state + " " + td.city);
+        });
     }
 
     @Test
