cc014750efddf9f25567d2183c35297f9d74ff7a
igorBys <stevejobs69@MacBook-Air-steve.local>
2025-04-21 12:21:31 +0300

    added screenshot

--- src/test/java/lesson11/tests/TestBase.java
+++ src/test/java/lesson11/tests/TestBase.java
@@ -6,6 +6,9 @@ import io.qameta.allure.selenide.AllureSelenide;
 import lesson11.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -18,6 +21,13 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
