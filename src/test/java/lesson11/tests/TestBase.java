cbaf64cd5a6330059df09bf10c71d84b249c69b0
igorBys <stevejobs69@MacBook-Air-steve.local>
2025-04-21 09:21:07 +0300

    added screenshot

--- src/test/java/lesson11/tests/TestBase.java
+++ src/test/java/lesson11/tests/TestBase.java
@@ -23,5 +23,7 @@ public class TestBase {
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
